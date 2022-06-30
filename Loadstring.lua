95b9ed0413cb2c2876220f1262fd72f47c21cee3
Kaoru <96850544+Uvxtq@users.noreply.github.com>
2022-06-30 19:13:54 -0400

    Updated

--- Loadstring.lua
+++ Loadstring.lua
@@ -1,1 +1,21 @@
+--[[
 
+___  ___               _                   _   _         _     
+|  \/  |              | |                 | | | |       | |    
+| .  . |  ___   _ __  | | __  ___  _   _  | |_| | _   _ | |__  
+| |\/| | / _ \ | '_ \ | |/ / / _ \| | | | |  _  || | | || '_ \ 
+| |  | || (_) || | | ||   < |  __/| |_| | | | | || |_| || |_) |
+\_|  |_/ \___/ |_| |_||_|\_\ \___| \__, | \_| |_/ \__,_||_.__/ 
+                                    __/ |                      
+                                   |___/                                                       
+Created by: oShy#3643.
+Developers: oShy#3643, Kaoru~#6438, mstudio45#5590, and tobaccoboy#2008.
+Version: 1.0.0
+Repository: https://github.com/Uvxtq/Monkey-Hub-Public
+Discord: https://discord.gg/9jWepyyWYA
+ChangeLog:
+        1.0.0:
+            - Initial release
+--]]
+
+loadstring(game:HttpGet(("https://raw.githubusercontent.com/Uvxtq/Monkey-Hub-Public/main/Loader.lua"),true))()
